5db2657b54a919532e9f2c46df987271ece74359
Sakshi <shriwastav.sakshi99@gmail.com>
2023-10-02 12:44:02 +0530

    Fetch Products

--- shopcart/src/app/store.js
+++ shopcart/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/products/productsSlice';
+import productsReducer from '../features/products/productsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: productsReducer,
   },
 });
--- shopcart/src/features/products/Products.js
+++ shopcart/src/features/products/Products.js
@@ -1,33 +1,31 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  fetchAsync,
-} from "./productsSlice";
+import { fetchAsync } from "./productsSlice";
 import "./Products.css";
 
 export function Products() {
-  
   const dispatch = useDispatch();
-  
+  const products = useSelector((state) => state.product.products);
   return (
     <div>
       <div>
         <button
-          
           aria-label="Decrement value"
           onClick={() => dispatch(fetchAsync())}
         >
           fetch Products
         </button>
-        <div className="card">
-          <img src="jeans3.jpg" alt="Denim Jeans" style={{width:"100%"}} />
-          <h1>Tailored Jeans</h1>
-          <p className="price">$19.99</p>
-          <p>Some text about the jeans..</p>
-          <p>
-            <button>Add to Cart</button>
-          </p>
-        </div>
+        {products.map((product) => (
+          <div className="card">
+            <img src={product.thumbnail} alt={product.title} style={{ width: "100%" }} />
+            <h1>{product.title}</h1>
+            <p className="price">{product.price}</p>
+            <p>{product.description}</p>
+            <p>
+              <button>Add to Cart</button>
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
